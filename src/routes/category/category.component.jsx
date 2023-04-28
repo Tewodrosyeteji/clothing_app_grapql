8f8e6b9ab21ae5ffd27f8c967cd7bf99f4deb284
Tewodrosyeteji <teddytejinesh@gmail.com>
2023-04-28 03:37:53 -0700

    frontEnd graphql

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,30 +1,57 @@
-import { useContext, useState, useEffect, Fragment } from 'react';
-import { useParams } from 'react-router-dom';
+import { useContext, useState, useEffect, Fragment } from "react";
+import { useParams } from "react-router-dom";
 
-import ProductCard from '../../components/product-card/product-card.component';
+import ProductCard from "../../components/product-card/product-card.component";
 
-import { CategoriesContext } from '../../contexts/categories.context';
+import { CategoriesContext } from "../../contexts/categories.context";
+import Spinner from "../../components/spinner/spinner.component";
+import { CategoryContainer, Title } from "./category.styles";
+import { gql, useQuery } from "@apollo/client";
 
-import { CategoryContainer, Title } from './category.styles';
-
+const CategoryGQL = gql`
+  query ($title: String!) {
+    getCollectionsByTitle(title: $title) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+  const { loading, error, data } = useQuery(CategoryGQL, {
+    variables: {
+      title: category,
+    },
+  });
 
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
-
+    if (data) {
+      const {
+        getCollectionsByTitle: { items },
+      } = data;
+      setProducts(items);
+    }
+  }, [category, data]);
+  const [products, setProducts] = useState([]);
   return (
     <Fragment>
       <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <CategoryContainer>
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      )}
     </Fragment>
   );
 };
